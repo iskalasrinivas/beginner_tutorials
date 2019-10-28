bd4d600b23ae2f65eb3f9a5454684722643349e4
iskalasrinivas <iskalasrinivas3@gmail.com>
2019-10-28 09:48:39 -0400

    [results] Added results for cpplint and cppcheck tests.

--- src/talker.cpp
+++ src/talker.cpp
@@ -7,10 +7,10 @@
  * on topic called chatter taken from ROS tutorials page.
  */
 
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-#include <sstream>
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
