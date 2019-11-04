4e2e01e889100a01ed9a6538a3e2d42aa9570627
iskalasrinivas <iskalasrinivas3@gmail.com>
2019-11-04 06:18:21 -0500

    [src/talker.cpp] Created a service call in the talker.cpp file. Added log messages and ability tochange frequency of sending messages via client input.

--- src/talker.cpp
+++ src/talker.cpp
@@ -29,12 +29,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  **/
 
-
 /**
  * @file talker.cpp
  * @author Raja Srinivas Iskala
  * @copyright 2019
  * @copyright BSD 3-Clause
+ * @date 04/11/2019
  * @brief This code is a Ros publisher node that publishes a string message
  * on topic called chatter taken from ROS tutorials page.
  */
@@ -42,12 +42,33 @@
 #include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "beginner_tutorials/changeBaseString.h"
 
+/**
+ * Default string message
+ */
+extern std::string defaultMessage = "Hello Raja!";
+
+/**
+ * @brief  callback function for the Service changeBaseString
+ * @param  requestService   The request that is sent to the service
+ * @param  responseClient   The response given by the service to the client
+ * @return bool
+ */
+bool changeString(
+    beginner_tutorials::changeBaseString::Request &requestService,
+    beginner_tutorials::changeBaseString::Response &responseClient) {
+  defaultMessage = requestService.baseString;
+  ROS_WARN_STREAM("Client has modified the default text to :");
+  responseClient.changedBaseString = requestService.baseString;
+  return true;
+}
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
 int main(int argc, char **argv) {
+
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -67,6 +88,32 @@ int main(int argc, char **argv) {
    */
   ros::NodeHandle n;
 
+  // declaring variable to denote loop frequency to 5 Hz default.
+  int rate = 5;
+
+  // converts string argument to int
+  if (argc > 1) {
+    rate = atoi(argv[1]);
+  }
+
+  if (rate > 0) {
+    ROS_DEBUG_STREAM("The loop is operating at frequency of Hz: " << rate);
+  } else if (rate < 0) {
+    ROS_ERROR_STREAM("The input frequency cannot be less than Zero");
+
+    ROS_WARN_STREAM("Correction: Defaulting frequency back to 5 Hz");
+
+    // Reset loopRate back to default (5 Hz)
+    rate = 5;
+  } else if (rate == 0) {
+    ROS_FATAL_STREAM("The input frequency cannot be Zero");
+
+    ROS_WARN_STREAM("Correction: Defaulting frequency back to 5 Hz");
+
+    // Reset loopRate back to default (5 Hz)
+    rate = 5;
+  }
+
   /**
    * The advertise() function is how you tell ROS that you want to
    * publish on a given topic name. This invokes a call to the ROS
@@ -84,10 +131,12 @@ int main(int argc, char **argv) {
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-  auto chatter_pub = n.advertise < std_msgs::String
-      > ("chatter", 1000);
+  auto chatter_pub = n.advertise < std_msgs::String > ("chatter", 1000);
 
-  ros::Rate loop_rate(10);
+  // calling advertiseService function with function callback changeString
+  auto server = n.advertiseService("changeBaseString", changeString);
+
+  ros::Rate loop_rate(rate);
 
   /**
    * A count of how many messages we have sent. This is used to create
